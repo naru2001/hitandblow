28cc387234d6c915c70f83afeafef044514cc2de
naru2001 <50087796+naru2001@users.noreply.github.com>
2021-02-11 16:22:25 +0900

    明日の自分がなんとかしてくれる

--- game_process.js
+++ game_process.js
@@ -7,6 +7,10 @@ function make_number(n) { //Fisher–Yates shuffle
     return num_array.slice(0, n);
 }
 
+function eval_Ineq(a, b, n) { //欲しかった
+    return a <= n && n <= b;
+}
+
 function ans_check(ans, reply) {
     let n = ans.length;
     let hit = 0;
@@ -22,11 +26,20 @@ function onClick(e) {
     var rect = e.target.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    check_coord(x,y);
+    check_coord(x, y);
+    console.log(x, y);
 }
 
-function check_coord(x,y){
-    //座標の判定する
+function check_coord(x, y) {
+    if (50.0 <= x <= 200.0 && 250.0 <= y <= 450.0) {
+        console.log(3);
+    }
+    else if (250.0 <= x <= 400.0 && 250.0 <= y <= 450.0) {
+        console.log(4);
+    }
+    else if (450.0 <= x <= 600.0 && 250.0 <= y <= 450.0) {
+        console.log(5);
+    }
     return
 }
 
@@ -34,3 +47,4 @@ window.addEventListener('load', function () {
     var canvas = $("gameCanvas");
     canvas.addEventListener('click', onClick, false);
 })
+
