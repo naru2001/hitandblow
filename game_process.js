7a3600228336d3b4266c0fd1be30d37a93eb56e2
nkgw <s1832099lh@s.chibakoudai.jp>
2021-02-12 21:22:47 +0900

    kari

--- game_process.js
+++ game_process.js
@@ -1,58 +1,81 @@
-function make_number(n) { //Fisher–Yates shuffle
-    const num_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let i = num_array.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        num_array[i] = [num_array[j], num_array[j] = num_array[i]][0];
-    }
-    return num_array.slice(0, n);
+var flag = 0; // 0->Menu, 1->3keta
+
+function make_number(n) {
+  //Fisher–Yates shuffle
+  const num_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = num_array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    num_array[i] = [num_array[j], (num_array[j] = num_array[i])][0];
+  }
+  return num_array.slice(0, n);
 }
 
 function eval_Ineq(a, b, n) {
-    return a <= n && n <= b;
+  return a <= n && n <= b;
 }
 
 function ans_check(ans, reply) {
-    let n = ans.length;
-    let hit = 0;
-    let brow = 0;
-    for (let i = 0; i < n; i++) {
-        if (ans[i] === reply[i]) hit++;
-        else if (ans.indexOf(reply[i]) >= 0) brow++;
-    }
-    return [hit, brow];
+  let n = ans.length;
+  let hit = 0;
+  let brow = 0;
+  for (let i = 0; i < n; i++) {
+    if (ans[i] === reply[i]) hit++;
+    else if (ans.indexOf(reply[i]) >= 0) brow++;
+  }
+  return [hit, brow];
 }
 
 function onClick(e) {
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
-    check_coord(x, y);
-    console.log(x, y);
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  check_coord(x, y);
+  //console.log(x, y);
 }
 
 function check_coord(x, y) {
+  if (flag == 0) {
     if (eval_Ineq(50.0, 200.0, x) && eval_Ineq(250.0, 450.0, y)) {
-        console.log(3);
+      console.log(3);
+      flag = 1;
+    } else if (eval_Ineq(250.0, 400.0, x) && eval_Ineq(250.0, 450.0, y)) {
+      console.log(4);
+      flag = 2;
+    } else if (eval_Ineq(450.0, 600.0, x) && eval_Ineq(250.0, 450.0, y)) {
+      console.log(5);
+      flag = 3;
+    } else if (eval_Ineq(50.0, 250.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("rule");
+      flag = 4;
+    } else if (eval_Ineq(280.0, 480.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("rank");
+      flag = 5;
+    } else if (eval_Ineq(520.0, 600.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("share");
     }
-    else if (eval_Ineq(250.0, 400.0, x) && eval_Ineq(250.0, 450.0, y)) {
-        console.log(4);
+  }else if(flag == 1){
+    if (eval_Ineq(50.0, 200.0, x) && eval_Ineq(250.0, 450.0, y)) {
+      console.log(6);
+      flag = 1;
+    } else if (eval_Ineq(250.0, 400.0, x) && eval_Ineq(250.0, 450.0, y)) {
+      console.log(7);
+      flag = 2;
+    } else if (eval_Ineq(450.0, 600.0, x) && eval_Ineq(250.0, 450.0, y)) {
+      console.log(8);
+      flag = 3;
+    } else if (eval_Ineq(50.0, 250.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("rule1");
+      flag = 4;
+    } else if (eval_Ineq(280.0, 480.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("rank2");
+      flag = 5;
+    } else if (eval_Ineq(520.0, 600.0, x) && eval_Ineq(510.0, 590.0, y)) {
+      console.log("share3");
     }
-    else if (eval_Ineq(450.0, 600.0, x) && eval_Ineq(250.0, 450.0, y)) {
-        console.log(5);
-    }
-    else if (eval_Ineq(50.0, 250.0, x) && eval_Ineq(510.0, 590.0, y)) {
-        console.log("rule");
-    }
-    else if (eval_Ineq(280.0, 480.0, x) && eval_Ineq(510.0, 590.0, y)) {
-        console.log("rank");
-    }
-    else if (eval_Ineq(520.0, 600.0, x) && eval_Ineq(510.0, 590.0, y)) {
-        console.log("share");
-    }
+  }
 }
 
-window.addEventListener('load', function () {
-    var canvas = $("gameCanvas");
-    canvas.addEventListener('click', onClick, false);
-})
-
+window.addEventListener("load", function () {
+  var canvas = $("gameCanvas");
+  canvas.addEventListener("click", onClick, false);
+});
